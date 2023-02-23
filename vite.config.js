a5740d2cc6e1e6348598f01b070391c33c27ebde
vdevalero <vdevalero44@gmail.com>
2023-02-23 23:36:45 +0100

    vite.congig.js update and added url repository

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080,
-   host: "192.168.1.21",
-  },
+  base: `/my-portfolio-frontend`,
+  // server: {
+  //   port: 8080,
+  //  host: "192.168.1.21",
+  // },
 });
