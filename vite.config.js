f3f45cc71d0dde8147f177117b2eea3bfc64a9b9
vdevalero <vdevalero44@gmail.com>
2023-02-25 11:49:40 +0100

    comment base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/my-portfolio-frontend`,
+  // base: `/my-portfolio-frontend`,
   // server: {
   //   port: 8080,
   //  host: "192.168.1.21",
