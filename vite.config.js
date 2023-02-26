7e053a799b7f26034582cca4b4872bcacade2868
vdevalero <vdevalero44@gmail.com>
2023-02-26 15:27:41 +0100

    Udating grid layout project view

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: `/my-portfolio-frontend`,
-  // server: {
-  //   port: 8080,
-  //  host: "192.168.1.21",
-  // },
+   server: {
+     port: 8080,
+    host: "192.168.1.21",
+   },
 });
